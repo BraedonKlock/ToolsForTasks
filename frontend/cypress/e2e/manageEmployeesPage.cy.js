4895d1566ac059f81925a344f86f38a1761a784a
Braedon Klock <kloc0004@algonquinlive.com>
2026-01-04 08:47:28 -0500

    added cypress test in manageEmployeesPage.cy.js to test that  edit employee with a id that already exists fails and displays proper error message, also updated the expected error message for other tests in the file

--- frontend/cypress/e2e/manageEmployeesPage.cy.js
+++ frontend/cypress/e2e/manageEmployeesPage.cy.js
@@ -41,7 +41,31 @@ describe("Jobs page (mobile)", () => {
         cy.get("#addEmployeePage-addBtn").click();
 
         cy.url().should("include", "/loggedIn/add-employee");
-        cy.get("#error").should("have.text", "Email already exists for this company.")
+        cy.get("#error").should("have.text", "Email or id already exists for this company.")
+    })
+
+
+    it("fails to edit Test employee when an id already exists and displays the proper error message", () => {
+        cy.url().should("include", "/loggedIn/manage-employees");
+
+        cy.contains(".employee-card", "Test")
+            .should("exist")
+            .as("testCard");
+
+        cy.get("@testCard").within(() => {
+            cy.get(".three-dot-menu-icon").click();
+            cy.get('a[href^="/loggedIn/edit-employee/"]').click();
+        });
+
+        cy.url().should("include", "/loggedIn/edit-employee/");
+
+
+        cy.get("#employeeid").clear().type("2");
+
+        cy.contains("button", /update/i).click();
+
+        cy.url().should("include", "/loggedIn/edit-employee");
+        cy.get("#error").should("have.text", "Email or id already exists for this company.")
     })
 
     it("edits Test employee and changes the name to Test Edited", () => {
